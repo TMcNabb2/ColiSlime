9637b90a51a2c39cadd6e1a38ea73d003b1218bb
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2025-02-18 14:40:18 -0700

    Updated stats system to not use reflection

--- Assets/Scripts/Stats/StatModifier.cs
+++ Assets/Scripts/Stats/StatModifier.cs
@@ -1,78 +1,62 @@
 using System;
-using UnityEditor.Search;
-using UnityEngine;
 
 namespace JDoddsNAIT.Stats
 {
-    public abstract class StatModifier : IDisposable
-    {
-        /// <summary>
-        /// Is true if the modifier is about to be removed.
-        /// </summary>
-        public bool MarkedForRemoval { get; private set; }
+	public interface IStatModifier : IDisposable
+	{
+		/// <summary>
+		/// Handles an incoming <paramref name="query"/>.
+		/// </summary>
+		/// <param name="sender">The <see cref="Stats{TStats}"/> object that requested the query.</param>
+		/// <param name="query">The <see cref="Query"/> object, holding the value to be modified.</param>
+		public void Handle(object sender, Query query);
+		/// <summary>
+		/// This event is invoked as the modifier is being removed.
+		/// </summary>
+		public event Action<IStatModifier> OnDispose;
+	}
 
-        /// <summary>
-        /// This event is invoked as the modifier is being removed.
-        /// </summary>
-        public event Action<StatModifier> OnDispose = delegate { };
+	public abstract class StatModifier<T> : IStatModifier
+	{
+		/// <summary>
+		/// Is true if the modifier is about to be removed.
+		/// </summary>
+		public bool MarkedForRemoval { get; private set; }
 
-        readonly CountdownTimer _timer;
+		public event Action<IStatModifier> OnDispose = delegate { };
+		//public event Action<StatModifier<T>> OnDispose = delegate { };
 
-        protected StatModifier(float duration)
-        {
-            if (duration <= 0) return;
+		readonly CountdownTimer _timer;
 
-            _timer = new(duration);
-            _timer.OnStop += _ => Dispose();
-            _timer.Start();
-        }
+		protected StatModifier(float duration)
+		{
+			if (duration <= 0) return;
 
-        /// <summary>
-        /// Handles an incoming <paramref name="query"/>.
-        /// </summary>
-        /// <param name="sender">The <see cref="Stats{TStats}"/> object that requested the query.</param>
-        /// <param name="query">The <see cref="Query"/> object, holding the value to be modified.</param>
-        public void Handle(object sender, Query query)
-        {
-            OnHandle(sender, query);
-        }
+			_timer = new(duration);
+			_timer.OnStop += _ => Dispose();
+			_timer.Start();
+		}
 
-        /// <summary>
-        /// Method determines how the incoming value will be modified.
-        /// </summary>
-        /// <param name="sender">The <see cref="Stats{TStats}"/> object that requested the query.</param>
-        /// <param name="query">The <see cref="Query"/> object, holding the value to be modified.</param>
-        protected abstract void OnHandle(object sender, Query query);
+		public void Handle(object sender, Query query)
+		{
+			if (query.Convert(out Query<T> q))
+			{
+				OnHandle(sender, q);
+			}
+		}
 
-        public void Dispose()
-        {
-            MarkedForRemoval = true;
-            _timer?.Dispose();
-            OnDispose?.Invoke(this);
-        }
-    }
+		/// <summary>
+		/// Method determines how the incoming value will be modified.
+		/// </summary>
+		/// <param name="sender">The <see cref="Stats{TStats}"/> object that requested the query.</param>
+		/// <param name="query">The <see cref="Query"/> object, holding the value to be modified.</param>
+		protected abstract void OnHandle<TQuery>(object sender, Query<TQuery> query);
 
-    /// <summary>
-    /// Basic modifier that performs an operation on the value.
-    /// </summary>
-    /// <typeparam name="T">The values type.</typeparam>
-    public class BasicStatModifier<T> : StatModifier
-    {
-        private readonly string _name;
-        protected readonly Func<T, T> _operation;
-
-        public BasicStatModifier(string name, Func<T, T> operation, float duration = 0) : base(duration)
-        {
-            _name = name;
-            _operation = operation;
-        }
-
-        protected override void OnHandle(object sender, Query query)
-        {
-            if (query.PropertyName == _name && query.Value is T value)
-            {
-                query.Value = _operation(value);
-            }
-        }
-    }
+		public void Dispose()
+		{
+			MarkedForRemoval = true;
+			_timer?.Dispose();
+			OnDispose?.Invoke(this);
+		}
+	}
 }
--- Assets/Scripts/Stats/Stats.cs
+++ Assets/Scripts/Stats/Stats.cs
@@ -1,114 +1,86 @@
 using System;
-using System.Collections.Generic;
-using System.Reflection;
 using UnityEngine;
 
 namespace JDoddsNAIT.Stats
 {
-    /// <summary>
-    /// Performs queries on a set of base stats to apply modifiers to those stats. Derive from this class to specify the stat container.
-    /// </summary>
-    /// <typeparam name="TStats">The type of the container class that holds properties that can be modified.</typeparam>
-    public abstract class Stats<TStats> : MonoBehaviour where TStats : ScriptableObject, new()
-    {
-        private readonly Type _type = typeof(TStats);
-        private readonly StatMediator _mediator = new();
-        private readonly Dictionary<string, PropertyInfo> _properties = new();
+	public interface IStatContainer
+	{
+		Query GetBaseValue(string statName);
+	}
 
-        /// <summary>
-        /// The stat mediator in change of adding and removing modifiers. (Read only)
-        /// </summary>
-        public StatMediator Mediator => _mediator;
+	/// <summary>
+	/// Performs queries on a set of base stats to apply modifiers to those stats. Derive from this class to specify the stat container.
+	/// </summary>
+	/// <typeparam name="TStats">The type of the container class that holds properties that can be modified.</typeparam>
+	public abstract class Stats<TStats> : MonoBehaviour where TStats : ScriptableObject, IStatContainer, new()
+	{
+		//private readonly Type _type = typeof(TStats);
+		private readonly StatMediator _mediator = new();
 
-        [SerializeField] private TStats _baseStats;
-        /// <summary>
-        /// The base stats that will be modified. (Read only)
-        /// </summary>
-        public TStats BaseStats => _baseStats;
+		/// <summary>
+		/// The stat mediator in change of adding and removing modifiers. (Read only)
+		/// </summary>
+		public StatMediator Mediator => _mediator;
 
-        /// <summary>
-        /// Call base.Awake() if overriding this method.
-        /// </summary>
-        protected virtual void Awake()
-        {
-            foreach (var property in _type.GetProperties())
-            {
-                _properties.Add(property.Name, property);
-            }
+		[SerializeField] private TStats _baseStats;
+		/// <summary>
+		/// The base stats that will be modified. (Read only)
+		/// </summary>
+		public TStats BaseStats => _baseStats;
 
-            if (_baseStats == null)
-            {
-                _baseStats = new();
-            }
-        }
+		/// <summary>
+		/// Call base.Awake() if overriding this method.
+		/// </summary>
+		protected virtual void Awake()
+		{
+			if (_baseStats == null)
+			{
+				_baseStats = new();
+			}
+		}
 
-        /// <summary>
-        /// <inheritdoc cref="StatMediator.AddModifier(StatModifier)"/>
-        /// </summary>
-        /// <param name="modifier"></param>
-        public void AddModifier(StatModifier modifier) => Mediator.AddModifier(modifier);
-        /// <summary>
-        /// <inheritdoc cref="AddModifier(StatModifier)"/>
-        /// </summary>
-        /// <param name="modifiers"></param>
-        public void AddModifier(params StatModifier[] modifiers) => Mediator.AddModifier(modifiers);
+		/// <summary>
+		/// <inheritdoc cref="StatMediator.AddModifier(IStatModifier)"/>
+		/// </summary>
+		/// <param name="modifier"></param>
+		public void AddModifier(IStatModifier modifier) => Mediator.AddModifier(modifier);
+		/// <summary>
+		/// <inheritdoc cref="AddModifier(IStatModifier)"/>
+		/// </summary>
+		/// <param name="modifiers"></param>
+		public void AddModifier(params IStatModifier[] modifiers) => Mediator.AddModifier(modifiers);
 
-        /// <summary>
-        /// <inheritdoc cref="StatMediator.RemoveModifier(StatModifier)"/>
-        /// </summary>
-        /// <param name="modifier"></param>
-        public void RemoveModifier(StatModifier modifier) => Mediator.RemoveModifier(modifier);
+		/// <summary>
+		/// <inheritdoc cref="StatMediator.RemoveModifier(IStatModifier)"/>
+		/// </summary>
+		/// <param name="modifier"></param>
+		public void RemoveModifier(IStatModifier modifier) => Mediator.RemoveModifier(modifier);
 
-        /// <summary>
-        /// Retrieves the modified value of a property with a given <paramref name="name"/>.
-        /// </summary>
-        /// <param name="name"></param>
-        /// <returns>The modified value.</returns>
-        /// <exception cref="ArgumentOutOfRangeException"/>
-        public object GetStat(string name)
-        {
-            if (!_properties.TryGetValue(name, out var prop))
-            {
-                throw new ArgumentOutOfRangeException(nameof(name), $"No property found with the name {name}.");
-            }
+		/// <summary>
+		/// Retrieves the modified value of a property with a given <paramref name="name"/>.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="name"></param>
+		/// <returns>The modified value.</returns>
+		/// <exception cref="Exception"></exception>
+		public T GetStat<T>(string name)
+		{
+			var q = BaseStats.GetBaseValue(name);
+			if (!q.Convert(out Query<T> query))
+			{
+				throw new Exception("Cannot get stat; query type is invalid.");
+			}
 
-            var q = new Query(prop, prop.GetValue(_baseStats));
-            Mediator.PerformQuery(this, q);
-            return q.Value;
-        }
+			Mediator.PerformQuery(this, query);
+			return query.Value;
+		}
 
-        /// <summary>
-        /// <inheritdoc cref="GetStat(string)"/>
-        /// </summary>
-        /// <remarks>
-        /// Generic version of <seealso cref="GetStat(string)"/>.
-        /// </remarks>
-        /// <typeparam name="T">The type to cast the return value into.</typeparam>
-        /// <param name="name"></param>
-        /// <returns>The modified value, as type <typeparamref name="T"/>.</returns>
-        /// <exception cref="ArgumentOutOfRangeException"/>
-        /// <exception cref="ArgumentException"/>
-        public T GetStat<T>(string name)
-        {
-            if (!_properties.TryGetValue(name, out var prop))
-            {
-                throw new ArgumentOutOfRangeException(nameof(name), $"No property found with the name {name}.");
-            }
-
-            if (prop.PropertyType != typeof(T))
-            {
-                throw new ArgumentException($"Property \"{name}\" is not of type {typeof(T).Name}.");
-            }
-
-            return (T)GetStat(prop.Name);
-        }
-
-        /// <summary>
-        /// Call base.OnDestroy() if overriding this method.
-        /// </summary>
-        protected virtual void OnDestroy()
-        {
-            Mediator.Dispose();
-        }
-    }
+		/// <summary>
+		/// Call base.OnDestroy() if overriding this method.
+		/// </summary>
+		protected virtual void OnDestroy()
+		{
+			Mediator.Dispose();
+		}
+	}
 }
